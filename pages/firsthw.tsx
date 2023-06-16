50d42d3fe1ed1450746492d8a09164b6b34a55cb
AzureAD\홍정기 <andy960609@gmail.com>
2023-06-16 10:06:10 +0900

    숙제 페이지 생성

--- pages/firsthw.tsx
+++ pages/firsthw.tsx
@@ -4,7 +4,8 @@ import Layout from "../components/common/Layout";
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
-    <p>This is the about page</p>
+    <p>여기 과제 페이지
+    </p>
     <p>
       <Link href="/">Go home</Link>
     </p>
