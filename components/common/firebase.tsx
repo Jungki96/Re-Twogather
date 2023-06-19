37b0fae1c282a465176257fff6721f8b583dbb3b
Jungki <andy960609@gmail.com>
2023-06-19 23:42:11 +0900

    파이어 베이스 노션 2회차까지 확인

---
+++ components/common/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbrjcok_vqJTpvFcpdpMl4do3PazynCIg",
+  authDomain: "fir-test-a7b09.firebaseapp.com",
+  projectId: "fir-test-a7b09",
+  storageBucket: "fir-test-a7b09.appspot.com",
+  messagingSenderId: "460337334522",
+  appId: "1:460337334522:web:460e740541f0068ba82143",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
