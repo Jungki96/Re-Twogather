51251b026b6fe872f1c9d2e3b3280c82b2c65c10
AzureAD\홍정기 <andy960609@gmail.com>
2023-06-21 09:38:58 +0900

    type 수정

--- pages/first/index.tsx
+++ pages/first/index.tsx
@@ -1,28 +1,39 @@
 import { useState } from "react";
 import Layout from "../../components/common/Layout";
 
+// 왜 빨간 줄이 나오는데 작동이 되는거지?
+// const children: Props = "왜?";
+
 type Props = {
   children: string;
+  information: string[];
 };
 
-// type Information = { name: string; description: string };
-// const [info, setInformation] = useState<Information | null>(null);
+const [init, setInit] = useState({
+  title: "",
+  desc:"",
+});
 
-// 왜 빨간 줄이 나오는데 작동이 되는거지?
-const children: Props = "왜?";
+const [information, setInformation] = useState<string[]>([]);
 
-const First: React.FC = () => (
-  <Layout>
-    {children}
+const submitBtn = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const { name, value } = e.target as HTMLInputElement;
+  setInit({ ...init, [name]: value });
+}
+
+const First: React.FC<Props> = () => (
     <div>
-      <div>
-        제목 <input onClick={() => setInformation()} />
-      </div>
-      <div>
-        할일 <input />
-      </div>
+          <Layout>
+            <div>
+              <div>
+                제목 <input name="title" onChange={submitBtn} />
+              </div>
+              <div>
+                할일 <input name="desc" onChange={submitBtn} />
+              </div>
+            </div>
+      </Layout>
     </div>
-  </Layout>
 );
 
 export default First;
