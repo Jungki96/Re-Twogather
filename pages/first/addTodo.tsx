bbb240c29a9212d8cafc0d9f3c3b47fee00f9a4a
AzureAD\홍정기 <andy960609@gmail.com>
2023-06-22 09:30:44 +0900

    Add todo file 설정

---
+++ pages/first/addTodo.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+type Props = {
+    children: string;
+    information: string[];
+  };
+
+const AddTodo: React.FC<Props> = () = {
+
+}
+
+export default AddTodo
--- pages/first/index.tsx
+++ pages/first/index.tsx
@@ -1,45 +1,38 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Layout from "../../components/common/Layout";
-
-// 왜 빨간 줄이 나오는데 작동이 되는거지?
-// const children: Props = "왜?";
 
 type Props = {
   children: string;
   information: string[];
 };
 
-
-
 const First: React.FC<Props> = () => {
-
-  const initialData = {
+  const [init, setInit] = useState({
     title: "",
     desc: "",
-  };
-  
-  const [init, setInit] = useState(initialData);
-  
+  });
+
+  const [information, setInformation] = useState<string[]>([]);
+
   const submitBtn = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target as HTMLInputElement;
     setInit({ ...init, [name]: value });
-  }
+  };
 
-  return(
+  return (
     <div>
-          <Layout>
-            <form>
-              <div>
-                제목 <input name="title" onChange={submitBtn} />
-              </div>
-              <div>
-                할일 <input name="desc" onChange={submitBtn} />
-              </div>
-              <button>제출</button>
-            </form>
+      <Layout>
+        <div>
+          <div>
+            제목 <input name="title" onChange={submitBtn} />
+          </div>
+          <div>
+            할일 <input name="desc" onChange={submitBtn} />
+          </div>
+        </div>
       </Layout>
     </div>
-    );
-  }
+  );
+};
 
 export default First;
